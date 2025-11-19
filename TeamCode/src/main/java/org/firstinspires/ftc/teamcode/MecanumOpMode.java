ddeda872410334c05846c839ef2cbcdb665d0096
Callie Heinrichs <callieheinrichs08@gmail.com>
2025-11-19 15:52:50 -0600

    Added Vision Class and updated various features for better support.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
@@ -3,24 +3,39 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumMovement;
+import org.firstinspires.ftc.teamcode.mechanisms.VisionInterface;
 
 @TeleOp(name = "Mecanum Testing", group = "Testing")
 public class MecanumOpMode extends OpMode {
 
     private MecanumMovement mecanumController = new MecanumMovement();
 
+    private VisionInterface visionInterface = new VisionInterface();
+
     @Override
     public void init() {
         mecanumController.init(hardwareMap, telemetry);
+        visionInterface.init(hardwareMap);
     }
 
     @Override
     public void loop() {
-        mecanumController.driveFieldRelative(
-                -gamepad1.left_stick_y,
-                gamepad1.left_stick_x,
-                gamepad1.right_stick_x
-        );
+        if (gamepad1.left_bumper) {
+            mecanumController.driveFacingPoint(
+                    -gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    new Pose2D(DistanceUnit.INCH, 10, 10, AngleUnit.RADIANS, 0)
+            );
+        } else {
+            mecanumController.driveFieldRelative(
+                    -gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x
+            );
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/VisionInterface.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/VisionInterface.java
@@ -3,20 +3,57 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+import com.bylazar.camerastream.PanelsCameraStream;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.robotcore.external.function.Consumer;
 import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 public class VisionInterface {
+    Processor processor = new Processor();
+    PanelsCameraStream cameraStream = PanelsCameraStream.INSTANCE;
+    private AprilTagProcessor aprilTag;
+    VisionPortal visionPortal;
 
-    public class Processor implements CameraStreamSource, VisionProcessor {
-        private AtomicReference<Bitmap> lastFrame =
+    private Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 18, 0, 0);
+    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+
+    public void init(HardwareMap hardwareMap) {
+        visionPortal = new VisionPortal.Builder()
+                .addProcessor(processor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
+
+        cameraStream.startStream(processor, 30);
+    }
+
+    public void loop() {
+
+    }
+
+    public void stop() {
+        cameraStream.stopStream();
+    }
+
+    public static class Processor implements CameraStreamSource, VisionProcessor {
+        // MIGHT HAVE TO TURN BACK TO NON FINAL
+        private final AtomicReference<Bitmap> lastFrame =
                 new AtomicReference<>(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565));
 
         @Override
